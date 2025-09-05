97bbefb434cead72bb686e6146ab88f296d5f15d
Sachinbambhaniya18 <sp922113@gmail.com>
2025-09-06 01:03:13 +0530

    Entity Updated

--- src/main/java/com/krane/fakeit/data/MockEndPoint.java
+++ src/main/java/com/krane/fakeit/data/MockEndPoint.java
@@ -1,15 +1,25 @@
 package com.krane.fakeit.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "mock_requests")
 public class MockEndPoint {
-    @Id @GeneratedValue
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "mock_id")
     private Long id;
+
+    @Column(name = "path")
     private String path;
+
+    @Column(name = "method")
     private String method;
+
+    @Column(name = "status_code")
     private int statusCode;
+
+    @Lob
+    @Column(name = "response_body")
     private String responseBody;
 }
